815007f65f855441c382da8d2b268a1a52122a6b
kaua <kauasanttosgestoria@gmail.com>
2023-05-23 15:44:48 -0300

    Creating: AppError If server error, message specifies If client error, message specifies

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,11 @@
+const AppError = require('../utils/AppError')
 class usersController {
    create( request, response ) {
       const { name, email, password } = request.body;
+
+      if(!name) {
+         throw new AppError("Please, fill in the name!")
+      }
 
       return response.status(201).json({ name, email, password })
    }
